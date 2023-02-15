6e241f5f1bbddd092535adcaa9118737ccab5b6f
Nicolas Hoffmann <nicolasfhh1992@hotmail.com>
2023-02-14 22:12:00 -0300

    Criação do “banco de dados”

--- src/main/java/br/com/api/controllers/MoedasController.java
+++ src/main/java/br/com/api/controllers/MoedasController.java
@@ -1,5 +1,8 @@
 package br.com.api.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +12,8 @@ import br.com.api.controllers.requests.MoedaRequest;
 
 @RequestMapping("/api/moedas")
 public class MoedasController {
+	
+	private final List<String> BANCO_DE_MOEDAS = Arrays.asList("USD", "EUR", "BRL", "GBP", "CAD", "ARS", "CNY", "JPY", "MXN");
 	
 	@PostMapping
 	public ResponseEntity<String> novaMoeda(@RequestBody MoedaRequest moedaRequest) {
